2adeb177b0a64c129d551484e114be2e7d6f1dfe
Robert Thurnher <r.thurnher@gmail.com>
2011-08-28 17:58:57 +0200

    Another minor code style refinement.

--- src/test/scala/spec.scala
+++ src/test/scala/spec.scala
@@ -4,8 +4,7 @@ import com.codahale.simplespec.Spec
 
 class AppSpec extends Spec {
   class `The greeter` {
-    @Test def `greets friendly.` = {
+    @Test def `greets friendly.` =
       Greeter.greet must be("Hello, Scala world!")
-    }
   }
 }
